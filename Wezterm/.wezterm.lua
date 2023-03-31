86d10395b9030b83a7dc7e4ac4473c72c27fb5ad
사무엘 <kor_pixel@fastmail.com>
2023-03-31 14:46:39 +0900

    Update .wezterm.lua
    refactored the code

--- Wezterm/.wezterm.lua
+++ Wezterm/.wezterm.lua
@@ -1,33 +1,38 @@
-local wezterm = require 'wezterm'
+
+local wezterm = require "wezterm"
 local config = {
-  font = wezterm.font 'D2Coding ligature',
-  font_size = 15,
-  hide_tab_bar_if_only_one_tab = true
-}
-config.color_scheme = 'seoulbones_dark'
-config.colors = {
-  cursor_fg = 'black'
-}
-config.colors = {
-  tab_bar = {
-    inactive_tab_edge = '#4b4b4b'
-  }
-}
-config.window_frame = {
-    inactive_titlebar_bg = '#4b4b4b',
-    active_titlebar_bg = '#4b4b4b',
-    -- These are fixes for wayland window decoration issue
-    inactive_titlebar_border_bottom = "#4b4b4b",
-    active_titlebar_border_bottom = "#4b4b4b",
-}
-config.window_padding = {
-  top = 0,
-}
-config.keys = {
-      {
-      key = 'q',
-      mods = 'CMD',
-      action = wezterm.action.CloseCurrentTab { confirm = true },
+
+    color_scheme = "seoulbones_dark",
+    font = wezterm.font "D2Coding ligature",
+    font_size = 15,
+    hide_tab_bar_if_only_one_tab = true,
+
+    colors = {
+        cursor_fg = "black",
+        tab_bar = {
+            inactive_tab_edge = "#4b4b4b"
+        }
     },
+
+    window_frame = {
+        inactive_titlebar_bg = "#4b4b4b",
+        active_titlebar_bg = "#4b4b4b",
+        -- These are fixes for wayland window decoration issue
+        inactive_titlebar_border_bottom = "#4b4b4b",
+        active_titlebar_border_bottom = "#4b4b4b"
+    },
+
+    window_padding = {
+        top = 0
+    },
+
+    keys = {
+        {
+            key = "q",
+            mods = "CMD",
+            action = wezterm.action.CloseCurrentTab {confirm = true}
+        }
+    }
+    
 }
 return config
